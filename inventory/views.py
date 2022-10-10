79ceebda7e68f1f4886c300227d861dab8894c24
jhammad <javier.hammad@gmail.com>
2022-10-10 13:07:48 +0100

    First view for the ingredeints list

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+
+from models import Ingredients, MenuItem, RecipeRequirement, Purchases
+
+@login_required(login_url='login')
+def dashboard(request):
+    total_ingredients = Ingredients.objects.count()
+ 
+
+    return render(request, 'dashboard.html', context)
