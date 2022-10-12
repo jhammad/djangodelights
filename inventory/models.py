0f565eec7b1de75a588750a53d83cbf0ad5fde53
jhammad <javier.hammad@gmail.com>
2022-10-12 22:30:26 +0100

    units changed in the models

--- inventory/models.py
+++ inventory/models.py
@@ -5,33 +5,37 @@ from django.db.models import Count, F, Value
 
 # Ingredients model
 class Ingredient(models.Model):
-    name = models.CharField(max_length=30)
-    quantity = models.IntegerField(default=0)
+    name = models.CharField(max_length=300)
+    quantity = models.Floatfield(default=0)
     unit = models.CharField(max_length=30)
     unit_price = models.FloatField(default=0) 
+    
+    def quantity_example(self):
+        return self.quantity * self.quantity
+    
     def __str__(self):
         return self.name 
     
 # Menu items model
 class MenuItem(models.Model):
     title = models.CharField(max_length=30)
-    price = models.IntegerField(default=0)
+    price = models.FloatField(default=0)
     
     def __str__(self):
         return self.title
 
 # Recipes model
 class RecipeRequirement(models.Model):
-    menu_item = models.ForeignKey(MenuItem, default=1, verbose_name= "menu items", on_delete=models.SET_DEFAULT)
-    Ingredient = models.ForeignKey(Ingredient, default=1, verbose_name= "ingredients", on_delete=models.SET_DEFAULT)
-    quantity = models.IntegerField(default=0)
+    menu_item = models.ForeignKey(MenuItem, default=1, verbose_name= "menu items", on_delete=models.CASCADE)
+    Ingredient = models.ForeignKey(Ingredient, default=1, verbose_name= "ingredients", on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
         
     #def __str__(self):
         #return self.menu_item 
 
 #Purchases 
 class Purchases(models.Model):
-    menu_item = models.ForeignKey(MenuItem, default=1, verbose_name= "menu items", on_delete=models.SET_DEFAULT)
+    menu_item = models.ForeignKey(MenuItem, default=1, verbose_name= "menu items", on_delete=models.CASCADE)
     Timestamp = models.DateTimeField(verbose_name=("Creation date"), auto_now_add=True, null=True)
     
     #def __str__(self):
