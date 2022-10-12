95b69ab91223ab5d526410edb64e7ecb546d9b0c
jhammad <javier.hammad@gmail.com>
2022-10-12 10:45:34 +0100

    mathfilters installed

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/javierhammad/python/La_Canaria/djangodelights/venv"
+set -gx VIRTUAL_ENV "/Users/javierhammad/python/La Canaria/djangodelights/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
